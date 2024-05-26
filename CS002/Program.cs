75987f5f41fe759b53d4c419d81affecfb29bb74
nhat0120230 <nhatnguyen22102003@gmail.com>
2024-05-26 19:36:34 +0700

    CS002

---
+++ CS002/Program.cs
@@ -0,0 +1,19 @@
+﻿// string studentName = "Abc";
+// int student_Age;
+// char a;
+// bool b;
+// float c;
+// c = (float)12.12;
+// object d;
+// d = studentName;
+
+// double so_pi = 3.14;
+// double hai_pi = 3.14 * so_pi;
+
+Console.Title = "vd su dung bien";
+Console.Clear();
+// Console.BackgroundColor = ConsoleColor.Blue;
+// Console.ForegroundColor = ConsoleColor.Blue;
+Console.ResetColor();
+
+Console.WriteLine("Xin chao day la chuong trinh su dung console");
